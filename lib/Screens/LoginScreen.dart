1a7a63691112782f76fe44f1d29c2ba78b319edd
yousufmyk <yk445261@gmail.com>
2023-02-25 02:33:44 +0400

    made inital intro Screen

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -10,6 +10,30 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            //borderRadius: BorderRadius.circular(40),
+            gradient: const RadialGradient(
+              center: AlignmentDirectional.topEnd,
+              //center: Alignment(-0.8, -0.8),
+              colors: [Color(0xFF1d1963), Color(0XFF3a1362)],
+              radius: 1.5,
+            ),
+            // borderRadius: BorderRadius.circular(300),
+            //image: DecorationImage(
+            // image:
+            //   AssetImage("Assets/Images/crpytorBackgroundImage.png"))),
+          ),
+          child: Column(
+            children: [
+              Image(
+                  //width: 400,
+                  image: AssetImage("Assets/Images/cloudsPic (1).png")),
+            ],
+          )),
+    );
   }
 }
